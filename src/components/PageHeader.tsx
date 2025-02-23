2ac11e6e33a9991bee0790b9db7db7c398267d68
DananjayaBC <dana.chanuka@gmail.com>
2025-02-23 14:47:20 +0530

    registarion form add redirect to home button

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -10,20 +10,22 @@ export default function PageHeader({
   return (
     <div className="flex flex-col items-center space-y-4">
       {/* SVG Logo */}
-      <svg
-        className="h-16 w-16 md:h-24 md:w-24 text-white" // Adjust size and color
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-        />
-      </svg>
+      <a href="https://earthytabs-landing.vercel.app/">
+        <svg
+          className="h-16 w-16 md:h-24 md:w-24 text-white" // Adjust size and color
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+          />
+        </svg>
+      </a>
       {/* Title */}
       <h1 className="mb-4 text-2xl font-semibold text-slate-300 md:text-5xl">
         {title}
